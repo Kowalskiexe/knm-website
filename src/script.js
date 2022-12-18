2cc8a9d9090bcda1150c5ec44bf93cc8171503a3
Kowalskiexe <kowalski.exe@gmail.com>
2022-12-18 14:26:19 +0100

    mobile version content update black social logos simplified main logo

--- src/script.js
+++ src/script.js
@@ -6,3 +6,25 @@ function navigate(url) {
 function navigateNewTab(url) {
     window.open(url, '_blank');
 }
+
+navbarExpanded = false;
+function expandNavbar() {
+    menu = document.getElementById('menu');
+    console.log('expand');
+    console.log(menu);
+    menu.classList.add('open');
+    navbarExpanded = true;
+}
+
+function collapseNavbar() {
+    menu = document.getElementById('menu');
+    console.log('collapse');
+    console.log(menu);
+    menu.classList.remove('open');
+    navbarExpanded = false;
+}
+
+function toggleNavbar() {
+    console.log('toggling');
+    navbarExpanded ? collapseNavbar() : expandNavbar();
+}
