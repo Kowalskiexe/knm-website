dd5a9255ba9ead7440caecc5f15b5c5909b57cb2
Kowalskiexe <kowalski.exe@gmail.com>
2023-02-23 13:53:22 +0100

    removed console.logs

--- src/script.js
+++ src/script.js
@@ -1,6 +1,5 @@
 function navigate(url) {
     window.location = url;
-    console.log(url);
 }
 
 function navigateNewTab(url) {
@@ -10,22 +9,17 @@ function navigateNewTab(url) {
 navbarExpanded = false;
 function expandNavbar() {
     menu = document.getElementById('menu');
-    console.log('expand');
-    console.log(menu);
     menu.classList.add('open');
     navbarExpanded = true;
 }
 
 function collapseNavbar() {
     menu = document.getElementById('menu');
-    console.log('collapse');
-    console.log(menu);
     menu.classList.remove('open');
     navbarExpanded = false;
 }
 
 function toggleNavbar() {
-    console.log('toggling');
     navbarExpanded ? collapseNavbar() : expandNavbar();
 }
 
